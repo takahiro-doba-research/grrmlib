492a2d9a7cce6359ea3093ac528e6dcb425e3e3a
Takahiro Doba <127459014+takahiro-doba-research@users.noreply.github.com>
2025-10-23 20:07:11 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from .min import MIN, read_min
 from .eq_list import EQList, read_eq_list
 from .pt_list import PTList, read_pt_list
 from .reaction_path_network import ReactionPathNetwork
-from .geometry import get_adj_matrix
+from .geometry import get_adj_matrix, get_distance, get_dihedral_angle
--- geometry.py
+++ geometry.py
@@ -15,3 +15,28 @@ def get_adj_matrix(symbols, atomcoords, threshold=1.25):
 def get_distance(atomcoords, label0, label1):
     d = np.sqrt(np.sum((atomcoords[label0 - 1] - atomcoords[label1 - 1]) ** 2))
     return d
+
+
+def get_dihedral_angle(atomcoords, label0, label1, label2, label3):
+    """
+    Returns dihedral angle in °
+    Assumes A-B-C-D connection and planes of ABC and BCD.
+    """
+    A = atomcoords[label0 - 1]
+    B = atomcoords[label1 - 1]
+    C = atomcoords[label2 - 1]
+    D = atomcoords[label3 - 1]
+    
+    n0 = np.cross(B - A, C - A)
+    n1 = np.cross(C - B, D - B)
+    
+    n0 /= np.linalg.norm(n0)
+    n1 /= np.linalg.norm(n1)
+    cos_theta = np.dot(n0, n1)
+    cos_theta = np.clip(cos_theta, -1.0, 1.0)  # 数値誤差対策
+    theta = np.arccos(cos_theta)
+    
+    sign = np.sign(np.dot(np.cross(n0, n1), C - B))
+    theta *= sign
+    angle = np.degrees(theta)
+    return angle
