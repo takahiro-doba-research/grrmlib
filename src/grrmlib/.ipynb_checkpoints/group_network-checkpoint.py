178f74aaf59c1707b51d89f71f2cdfd6b8157a61
takahiro-doba-research <takahiro.doba.research@gmail.com>
2026-01-14 09:24:04 +0900

    Update rpn and gn

---
+++ src/grrmlib/.ipynb_checkpoints/group_network-checkpoint.py
@@ -0,0 +1,45 @@
+import networkx as nx
+
+from grrmlib import EQs, PTs
+
+
+class GroupNetwork:
+    
+    def __init__(self):
+        self._G = nx.Graph()
+    
+    def __getattr__(self, name):
+        return getattr(self._G, name)
+    
+    def to_networkx(self):
+        return self._G
+    
+    def add_rpn(self, rpn):
+        nodes = []
+        for n, eq in rpn.nodes(data="EQ"):
+            n_group = eq.group
+            for node in nodes:
+                if node[0] == n_group:
+                    node[1]["EQs"][n] = eq
+                    break
+            else:
+                nodes.append((n_group, {"EQs": EQs({n: eq})}))
+        self._G.add_nodes_from(nodes)
+        
+        edges = []
+        for u, v, k, pt in rpn.edges(keys=True, data="PT"):
+            u_group = rpn.nodes[u]["EQ"].group
+            v_group = rpn.nodes[v]["EQ"].group
+            for edge in edges:
+                if set(edge[:2]) == {u_group, v_group}:
+                    edge[2]["PTs"][k] = pt
+                    break
+            else:
+                edges.append((u_group, v_group, {"PTs": PTs({k: pt})}))
+        self._G.add_edges_from(edges)
+    
+    def get_eqs(self, n):
+        return self._G.nodes[n]["EQs"]
+        
+    def get_pts(self, u, v):
+        return self._G.edges[u, v]["PTs"]
