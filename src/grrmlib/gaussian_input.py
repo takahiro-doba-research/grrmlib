6099cdc3d11b7bd065bed8be60032cbc7694a7a6
takahiro-doba-research <takahiro.doba.research@gmail.com>
2026-02-02 17:39:29 +0900

    Update .gitignore

---
+++ src/grrmlib/gaussian_input.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from .molecule import Molecule
+
+
+def read_gaussian_input(path):
+    
+    with open(path, "r") as f:
+        lines = f.readlines()
+    
+    indices_blank = [i for i, line in enumerate(lines) if line == "\n"]
+    header = lines[:indices_blank[1] + 2]
+    footer = lines[indices_blank[2]:]
+    lines_coord = lines[indices_blank[1] + 2:indices_blank[2]]
+    symbols = [line.split()[0] for line in lines_coord]
+    atomcoords = np.array([list(map(float, line.split()[1:4])) for line in lines_coord])
+    
+    notes = [list(map(int, line.split()[4:])) for line in lines_coord]
+    if all(not note for note in notes):
+        notes = None
+    
+    return Molecule(
+        symbols=symbols,
+        atomcoords=atomcoords,
+        notes=notes,
+        header=header,
+        footer=footer,
+    )
