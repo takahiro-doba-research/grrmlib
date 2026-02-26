0563f17286da911f109be45470899b57863d7c3c
takahiro-doba-research <takahiro.doba.research@gmail.com>
2026-02-26 10:43:02 +0900

    Update method: run

--- src/grrmlib/jobs/gaussian.py
+++ src/grrmlib/jobs/gaussian.py
@@ -6,18 +6,10 @@ class GaussianJob:
     
     def __init__(self, path_input: str | Path) -> None:
         self.path_input = Path(path_input)
-        self.path_output = self.path_input.with_suffix(".log")
         self.cwd = self.path_input.parent
     
     def run(self) -> None:
-        with (
-            open(self.path_input, "r") as f_in,
-            open(self.path_output, "w") as f_out
-        ):
-            subprocess.run(
-                ["g16"],
-                stdin=f_in,
-                stdout=f_out,
-                cwd=self.cwd,
-                check=True,
-            )
+        subprocess.run(
+            ["g16", self.path_input.name],
+            cwd=self.cwd,
+        )
