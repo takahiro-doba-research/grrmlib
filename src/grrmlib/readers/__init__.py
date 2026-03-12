f40410522d5bdb2b650947f2d60090a431e3cf3c
takahiro-doba-research <takahiro.doba.research@gmail.com>
2026-03-12 15:49:02 +0900

    Delete _optopt

--- src/grrmlib/readers/__init__.py
+++ src/grrmlib/readers/__init__.py
@@ -12,7 +12,6 @@ from .grrm import GRRMInputReader
 from .min import GRRMMINOutputReader
 from .eq_list import read_eq_list
 from .pt_list import read_pt_list
-from .lup_ts import LUPTS, read_lup_ts
 
 # Custom
 from .connectable import ConnectableReader, read_connectable
